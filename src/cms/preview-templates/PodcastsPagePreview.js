771dfa1119f1a1c1ab4e2cb8f82a828636bc844d
Jeff Yourman <jeffyourman@jeffs-mbp-2.lan>
2023-03-11 17:21:15 -0600

    iupdates

---
+++ src/cms/preview-templates/PodcastsPagePreview.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { PodcastsPageTemplate } from '../../templates/podcasts-page'
+
+const PodcastsPagePreview = ({ entry, widgetFor }) => (
+  <PodcastsPageTemplate
+    title={entry.getIn(['data', 'title'])}
+    content={widgetFor('body')}
+  />
+)
+
+PodcastsPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
+
+export default PodcastsPagePreview
